68b779eeb05394f9a16e9ca7e3bec6368eeb6768
nitarahmawati19 <nita.223040030@mail.unpas.ac.id>
2025-01-01 16:37:08 +0700

    category controller

---
+++ app/Http/Controllers/CetakController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            // $title = ' in ' . $category->name;
+
+        }
+
+        return view('categories', [
+            "title" => "Post Categories",
+            "categories" => Category::all(),
+            'posts' => Post::latest()->filter(request(['category']))->paginate(25)->withQueryString()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
