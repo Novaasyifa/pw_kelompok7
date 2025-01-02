df5c35cebff275886687d06b31ea5fc3095fd571
indritanialestari <indritanialestari@gmail.com>
2025-01-02 14:52:59 +0700

    halaman privacy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -20,20 +20,20 @@
         <p class="text-xs mt-2">Kebijakan Privasi dan Syarat Penggunaan di kmerchhub</p>
     </div><br>
                     <div class="px-5">
-                    <h4 class="font-bold text-[#584f49]"> 1. Kebijakan Privasi: </h4>
+                    <h4 class="font-bold text-[#584f49]"> 1. Kebijakan Privasi : </h4>
                     <ol class="border-b border-gray-700 gap-3 py-4 ">
-                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Pengumpulan Informasi: Saat Anda mengunjungi situs web kami, beberapa informasi mungkin secara otomatis dikumpulkan, seperti alamat IP Anda, jenis browser yang Anda gunakan, serta informasi lain yang berkaitan dengan interaksi Anda dengan situs kami.</li>
-                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Penggunaan Informasi: Informasi yang dikumpulkan dapat digunakan untuk meningkatkan kualitas layanan kami, memberikan rekomendasi yang sesuai, serta untuk keperluan analisis dan penelitian.</li>
-                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Kepamanan Data: Kami mengambil langkah-langkah keamanan yang sesuai untuk melindungi data pribadi Anda dari akses, penggunaan, atau pengungkapan yang tidak sah.</li>
+                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Pengumpulan Informasi : Saat Anda mengunjungi situs web kami, beberapa informasi mungkin secara otomatis dikumpulkan, seperti alamat IP Anda, jenis browser yang Anda gunakan, serta informasi lain yang berkaitan dengan interaksi Anda dengan situs kami.</li>
+                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Penggunaan Informasi : Informasi yang dikumpulkan dapat digunakan untuk meningkatkan kualitas layanan kami, memberikan rekomendasi yang sesuai, serta untuk keperluan analisis dan penelitian.</li>
+                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Kepamanan Data : Kami mengambil langkah-langkah keamanan yang sesuai untuk melindungi data pribadi Anda dari akses, penggunaan, atau pengungkapan yang tidak sah.</li>
                     </ol>
                 </div>
                     <br>
                     <div class="px-5">
-                    <h4 class="font-bold text-[#584f49]"> 2. Syarat Penggunaan: </h4>
+                    <h4 class="font-bold text-[#584f49]"> 2. Syarat Penggunaan : </h4>
                     <ol class="border-b border-gray-700 gap-3 py-4">
-                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Persetujuan: Dengan mengakses dan menggunakan situs web kami, Anda dianggap telah membaca, memahami, dan menyetujui semua syarat dan ketentuan yang tercantum di halaman ini.</li>
-                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Pembatasan Penggunaan: Anda setuju untuk tidak menggunakan situs kami untuk tujuan yang melanggar hukum, merugikan, mengancam, atau yang dapat merugikan hak atau kepentingan pihak lain.</li>
-                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Perubahan Syarat: Kami berhak untuk memperbarui, mengubah, atau menghapus bagian dari kebijakan privasi dan syarat penggunaan ini setiap saat tanpa pemberitahuan sebelumnya.</li>
+                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Persetujuan : Dengan mengakses dan menggunakan situs web kami, Anda dianggap telah membaca, memahami, dan menyetujui semua syarat dan ketentuan yang tercantum di halaman ini.</li>
+                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Pembatasan Penggunaan : Anda setuju untuk tidak menggunakan situs kami untuk tujuan yang melanggar hukum, merugikan, mengancam, atau yang dapat merugikan hak atau kepentingan pihak lain.</li>
+                        <li><span class="inline-block text-gray-600 align-middle">☻</span>Perubahan Syarat : Kami berhak untuk memperbarui, mengubah, atau menghapus bagian dari kebijakan privasi dan syarat penggunaan ini setiap saat tanpa pemberitahuan sebelumnya.</li>
                     </ol>
                 </div>
             </div>
